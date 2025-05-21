3ba90227b3b20d9d328439b4429c7da734e3fb27
moonboysui <lostenigmas2095@gmail.com>
2025-05-21 19:37:07 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 class Config:
     BOT_TOKEN = os.getenv("BOT_TOKEN")
-    BOOST_WALLET = os.getenv("BOOST_WALLET", "0x7338ef163ee710923803cb0dd60b5b02cddc5fbafef417342e1bbf1fba20e702")
-    TRENDING_CHANNEL = os.getenv("TRENDING_CHANNEL", "@moonbagstrending")
-    PORT = int(os.getenv("PORT", 10000))  # Render requires port 10000
+    BOOST_WALLET = os.getenv("BOOST_WALLET")
+    TRENDING_CHANNEL = os.getenv("TRENDING_CHANNEL")
+    PORT = int(os.getenv("PORT", 8080))  # Render requires port 10000
     WEB_SERVER_URL = os.getenv("WEB_SERVER_URL", "https://your-render-app-name.onrender.com")
   
