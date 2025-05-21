60ba58efd200e4bdd4521448688cfb08a6c4d856
moonboysui <lostenigmas2095@gmail.com>
2025-05-21 19:49:29 +0100

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ class Config:
     BOT_TOKEN = os.getenv("BOT_TOKEN")
     BOOST_WALLET = os.getenv("BOOST_WALLET")
     TRENDING_CHANNEL = os.getenv("TRENDING_CHANNEL")
-    PORT = int(os.getenv("PORT", 8080))  # Render requires port 10000
+    PORT = int(os.getenv("PORT", 10000))  # Render requires port 10000
     WEB_SERVER_URL = os.getenv("WEB_SERVER_URL", "https://your-render-app-name.onrender.com")
   
